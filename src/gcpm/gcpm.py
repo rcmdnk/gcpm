4a393d7e1d09ebfcda9419d46cbb3b5e363d261d
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-01-01 16:30:29 +0900

    import fire

--- src/gcpm/gcpm.py
+++ src/gcpm/gcpm.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
-
 from __future__ import print_function
+import fire
+
+
+class Gcpm(object):
+
+    def __init__(self, arg1):
+        self.arg1 = arg1
+
+    def test1(self, xxx):
+        print("test1", xxx, self.arg1)
+
+    def test2(self, xxx):
+        print("test2", xxx)
 
 
 def main():
-    print("gcpm")
+    fire.Fire(Gcpm)
 
 
 if __name__ == '__main__':
