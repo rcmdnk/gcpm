8054969a69fb084b1f2fda4d6620fbad8b6663e3
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-01-05 18:20:34 +0900

    fixed after_script at travis, version update function in manage.sh

--- manage.sh
+++ manage.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-commands=(test)
+commands=(test version)
 usage="Usage: $0 <command>
   commands: ${commands[*]}"
 
+
+version_update () {
+  version=$(grep "^version" pyproject.toml | cut -d '"' -f2)
+  echo "__version__ = \"$version\"" > ./src/gcpm/__version__.py
+  sed -i .bak "s/    assert __version__ == '.*'/    assert __version__ == '$version'/" tests/test_gcpm.py
+  rm -f tests/test_gcpm.py.bak
+}
+
 case $1 in
   test)poetry run pytest -v --cov=./src --cov-report=html;;
+  version)version_update;;
   *)echo "$usage";exit 1;;
 esac
