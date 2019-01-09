6c64d1583515afa40601160d0f94aaf17beb8478
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-01-09 16:25:19 +0900

    added test for proc

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,12 @@
+import os
 from gcpm import utils
 
 
 def test_expand():
     assert utils.expand("~/test") == utils.expand("$HOME/test")
+
+
+def test_proc():
+    (ret, stdout, stderr) = utils.proc(["hostname"])
+    assert ret == 0
+    assert stdout == os.environ["HOSTNAME"]
